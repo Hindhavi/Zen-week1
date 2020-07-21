d35407ca8462fc903ba9cf3acf27765dcfb7b8d8
Hindhavi Alluru <67951932+Hindhavi@users.noreply.github.com>
2020-07-21 23:12:39 +0530

    Add files via upload

---
+++ maxarray.js
@@ -0,0 +1,3 @@
+var arr = [16, 2, 8, 15, 19];
+var max = Math.max(...arr);
+console.log(max)
---
+++ sumarray.js
@@ -0,0 +1,5 @@
+var array = [12, 24, 13, 36, 11];
+var sum = array. reduce(function(a, b){
+return a + b;
+}, 0);
+console. log(sum);
